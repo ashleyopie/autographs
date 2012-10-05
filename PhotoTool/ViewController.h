5d244b435c5b9def306567fac5ba44a46fbc005d
indianic <indianic@ind274.local>
2012-10-05 10:21:24 +0530

    Initial Commit

---
+++ PhotoTool/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoTool
+//
+//  Created by indianic on 05/10/12.
+//  Copyright (c) 2012 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
